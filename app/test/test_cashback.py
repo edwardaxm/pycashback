720016f024f3b8bc5ee90cade840f85dd8b712f9
student <student@localhost>
2018-12-01 12:16:46 +0300

    100% coverage added

--- app/test/test_cashback.py
+++ app/test/test_cashback.py
@@ -5,3 +5,9 @@ def test_cashback_under_limit():
     result = cashback(1000)
 
     assert 50 == result
+
+
+def test_cashback_upper_limit():
+    result = cashback(100000)
+
+    assert 3000 == result
